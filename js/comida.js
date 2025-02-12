bad058cd7f76bfdcff65513fc8f9d9e9d4b77318
cayoss777 <cayoss.77@gmail.com>
2025-02-12 18:33:36 -0500

    CuerpoCarrito

---
+++ js/comida.js
@@ -0,0 +1,74 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const comida = [
+    {
+      id:1,
+      nombre: "Pan",
+      precio: 12,
+    },
+    {
+      id:2,
+      nombre: "Carne",
+      precio: 14,
+    },
+    {
+      id:3,
+      nombre: "Chocolate",
+      precio: 13,
+    },
+    {
+      id:4,
+      nombre: "Pan",
+      precio: 12,
+    },
+ 
+    {
+      id:6,
+      nombre: "Chocolate",
+      precio: 13,
+    },
+    {
+      id:7,
+      nombre: "Pan",
+      precio: 12,
+    },
+    {
+      id:8,
+      nombre: "Carne",
+      precio: 14,
+    },
+    {
+      id:9,
+      nombre: "Chocolate",
+      precio: 13,
+    },
+    {
+      id:10,
+      nombre: "Pan",
+      precio: 12,
+    },
+    {
+      id:11,
+      nombre: "Carne",
+      precio: 14,
+    },
+    {
+      id:12,
+      nombre: "Chocolate",
+      precio: 13,
+    },
+    {
+      id:13,
+      nombre: "Pan",
+      precio: 12,
+    },
+    {
+      id:14,
+      nombre: "Carne",
+      precio: 14,
+    },
+    {
+      id:15,
+      nombre: "Chocolate",
+      precio: 13,
+    },
+  ]
